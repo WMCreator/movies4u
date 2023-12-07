2644655762fee749e1ef7b252719f1d4cbed3b1d
WM Creator <wilsonmelendezfonseca@gmail.com>
2023-12-06 23:47:25 -0600

    Slider working

--- banner.js
+++ banner.js
@@ -1,2 +1,50 @@
-const banner = document.querySelector('.banner');
-const movies = [...banner.querySelectorAll('.banner__movie')];
+const bannerContainer = document.querySelector('.banner__container');
+const movieWidth = document.querySelector('.movie').offsetWidth;
+const dotsContainer = document.querySelector('.dots_container');
+const dots = dotsContainer.querySelectorAll('.dot');
+
+const interval = 5000; 
+
+function scrollSlider() {
+  const scrollPosition = bannerContainer.scrollLeft;
+
+  const newPosition = scrollPosition + movieWidth;
+
+  bannerContainer.scrollTo({
+    left: newPosition,
+    behavior: 'smooth'
+  });
+
+  if (newPosition >= bannerContainer.scrollWidth - bannerContainer.offsetWidth) {
+    bannerContainer.scrollTo({
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+}
+
+let scrollTimer = null;
+
+function identifyCurrentBanner() {
+    clearTimeout(scrollTimer);
+    
+    scrollTimer = setTimeout(() => {
+      currentBannerIndex = Math.floor(bannerContainer.scrollLeft / movieWidth);
+      
+      updateBannerDots();
+    }, 200); 
+  }
+  
+  function updateBannerDots() {
+    dots.forEach((dot, index) => {
+      if (index === currentBannerIndex) {
+        dot.classList.add('dot--active');
+      } else {
+        dot.classList.remove('dot--active');
+      }
+    });
+  }
+  
+bannerContainer.addEventListener('scroll', identifyCurrentBanner);
+
+setInterval(scrollSlider, interval);
